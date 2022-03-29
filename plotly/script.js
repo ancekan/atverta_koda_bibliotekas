0ea3320546e595c7867ef166af01d026309ee666
ancekan <58723696+ancekan@users.noreply.github.com>
2022-03-29 23:31:59 +0300

    Add files via upload

---
+++ plotly/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var data = [{
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
+
+});
